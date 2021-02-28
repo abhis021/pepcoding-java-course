128319281b1b08936d3d0f1640566d0f424f90d6
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-02-28 21:48:25 +0530

    commit on main

---
+++ starprint.java
@@ -0,0 +1,12 @@
+public class starprint {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("altenatively");
+        System.out.println("*\n**\n***\n****\n*****");
+    }
+    
+}
---
+++ sumoftwoint.java
@@ -0,0 +1,19 @@
+
+public class sumoftwoint {
+    public static void main(String[] args)
+    {
+        int x=15;
+        int y=10;
+        int sum= x+y;
+        int prd=x*y;
+        int v1=x/y;
+        int v2=y/x;
+        int v3=x%y;
+
+        System.out.println("Sum of " +x+" and "+y+" is "+sum);
+        System.out.println("Product of " +x+" and "+y+" is "+prd);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+    }
+}
