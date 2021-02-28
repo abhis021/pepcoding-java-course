6e7470589590f39d2d99476d4fce2d437dda02c0
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-02-28 22:26:00 +0530

    conditionals 1 and two of pepcode

---
+++ conditionals.java
@@ -0,0 +1,25 @@
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+public class conditionals {
+    
+public static void main(String[] args) {
+       int x;
+
+       System.out.println("Enter value of x");
+       Scanner xin = new Scanner(System.in);
+       
+       x =xin.nextInt();
+       
+       if(x%2 ==0)
+       {
+           System.out.println(x+" is even.");
+           System.out.println("Smart work is better than Hard work "); 
+       }
+       else{
+           System.out.println(x+ " is odd.");
+           System.out.println("Hard work is better than smart work"); 
+       }
+       
+    }
+}
---
+++ conditionals2.java
@@ -0,0 +1,21 @@
+public class conditionals2 {
+    public static void main(String[] args) {
+        int n1=20;
+        int n2=10;
+
+        if(n1==n2)
+        {
+            System.out.println(n1+" is equal to " + n2);
+        }
+        else if(n1>n2)
+        {
+            System.out.println(n1+" is greater than " +n2);
+        }
+        else
+        {
+            
+            System.out.println(n1+" is smaller than " +n2);
+
+        }
+    }
+}
