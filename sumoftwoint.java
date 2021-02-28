79323e79f0c4246e1e54b0a78001645a3590b370
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-02-28 21:52:25 +0530

    majedar coding

--- sumoftwoint.java
+++ sumoftwoint.java
@@ -9,11 +9,15 @@ public class sumoftwoint {
         int v1=x/y;
         int v2=y/x;
         int v3=x%y;
+        int exp=prd/sum;
 
         System.out.println("Sum of " +x+" and "+y+" is "+sum);
         System.out.println("Product of " +x+" and "+y+" is "+prd);
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
+
+        //exp xy/x+y
+        System.out.println(exp);
     }
 }
