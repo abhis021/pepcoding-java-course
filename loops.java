3eff9452170c9814762a42e72d74d393017e2f80
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-03-03 19:45:26 +0530

    while loop

---
+++ loops.java
@@ -0,0 +1,12 @@
+public class loops {
+    public static void main(String[] args) {
+        int i =1;
+
+        while(i<=9)
+        {
+            System.out.println(i);
+            i = i+1;
+        }
+        System.out.println("Done.");
+    }
+}
