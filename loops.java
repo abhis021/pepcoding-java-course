0115b88499f7a44e35df60afd302f66ed835c59a
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-03-03 19:51:49 +0530

    for loop

--- loops.java
+++ loops.java
@@ -1,12 +1,16 @@
 public class loops {
     public static void main(String[] args) {
-        int i =1;
+     /*   int i =1;
 
         while(i<=9)
         {
             System.out.println(i);
             i = i+1;
+        }*/
+        for(int i=0; i<=9; i++){
+            System.out.println(i);
         }
+
         System.out.println("Done.");
     }
 }
