36213ccbcaa494eba52daf0d5226f5e5b192b125
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-03-13 21:39:04 +0530

    grammatical correction, beautification

--- FirstNFibonacci.java
+++ FirstNFibonacci.java
@@ -5,16 +5,16 @@ public class FirstNFibonacci {
     public static void main(String[] args) {
     
         int t1=0, t2=1, nextTerm = 0, n;
-        System.out.println("Enter a positive number: ");
+        System.out.print("Enter a positive number: ");
         Scanner s =  new Scanner(System.in);
         n = s.nextInt();
         s.close();
 
-        System.out.println("Fibonacci Series:" +t1+" "+t2);
+        System.out.print("Fibonacci Series: " +t1+", "+t2);
         nextTerm = t1+t2;
 
         while(nextTerm<=n){
-            System.out.println(nextTerm);
+            System.out.print(", "+nextTerm);
             t1=t2;
             t2=nextTerm;
             nextTerm = t1+t2;
